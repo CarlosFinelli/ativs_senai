16fdbd4e771ed3a9dd8323feb895f11ed5faeb45
Carlos Guilherme Miranda Finelli <60704748+CarlosFinelli@users.noreply.github.com>
2021-04-14 21:23:46 -0300

    Conclusão das atividades pendentes

---
+++ aula29_CreateFile_Carlos/src/CreateFile.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.IOException;
+
+public class CreateFile {
+    public static void main(String[] args) {
+        try{
+            File myObj = new File("filename.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("Arquivo já existente");
+            }
+
+        } catch (IOException e) {
+        System.out.println("An error has ocurred");
+        e.printStackTrace();
+        }
+    }
+}
